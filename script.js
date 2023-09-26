d05270b40b05fd9876702d0e1409a1270a8f9a31
Charlie <131995134+weeklyrambler@users.noreply.github.com>
2023-09-26 16:14:46 +0100

    A grid of sorts can now be generated by the user

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const container = document.querySelector('#container');
 
 
 
-document.getElementById('btn').addEventListener("click", (number) => {
+const number = document.getElementById('btn').addEventListener("click", (number) => {
 
-    number = prompt("Please enter a number," ); 
-    
-    for (i = 0; i < number; i++) {
-      const div = document.createElement('div'); 
-      div.className.add("div"); 
-      container.appendChild("div"); 
-    }; 
+     number = prompt("Please enter a number: "); 
+     let sum = number * number; 
+     console.log(sum); 
 
-
+    return numOfDivs(sum); 
 }); 
+
+function numOfDivs (num) {
+    for (i = 0; i < num; i++) {
+        const div = document.createElement('div'); 
+        div.classList.add('gridDiv'); 
+        container.appendChild(div); 
+      }; 
+}
 
 //const gridSquare = document.createElement('div')
